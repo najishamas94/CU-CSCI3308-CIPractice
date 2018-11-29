06b5c8713f22a830c461862cdf129787f79c9dac
najishamas94 <naji.shamas@colorado.edu>
2018-11-28 18:32:43 -0700

    Adding test cases

--- geometry.h
+++ geometry.h
@@ -21,6 +21,6 @@ double coord_2d_dist(const coord_2d_t* a, const coord_2d_t* b);
 
 /* Test if two 2D coordinates are equal (e.g. withen 0.01 of each other) */
 bool coord_2d_eq(const coord_2d_t* a, const coord_2d_t* b);
-
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c);
 /* Calculate the midpoint between two 2D coordinates and load into mid */
 void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b);
